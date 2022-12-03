57ef8e058b336fe369436fdc25d5e0ebc000dbcd
salahbgm <116580792+salahbgm@users.noreply.github.com>
2022-12-03 17:31:58 +0200

    test

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -1,4 +1,14 @@
 public class Employee {
+    int employeeNo;
+    String name;
+    String gende;
+    String race;
 
+    public Employee(int employeeNo, String name, String gende, String race) {
+        this.employeeNo = employeeNo;
+        this.name = name;
+        this.gende = gende;
+        this.race = race;
+    }
 
 }
