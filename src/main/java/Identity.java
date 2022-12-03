5c0327deecbf0064b1ad25b8b9b0c84ae33f2d49
salahbgm <116580792+salahbgm@users.noreply.github.com>
2022-12-03 18:32:02 +0200

    class

--- src/main/java/Identity.java
+++ src/main/java/Identity.java
@@ -1,4 +1,25 @@
 public class Identity {
     private String passport;
     private int idNo;
+
+    public Identity(String passport, int idNo) {
+        this.passport = passport;
+        this.idNo = idNo;
+    }
+
+    public String getPassport() {
+        return passport;
+    }
+
+    public void setPassport(String passport) {
+        this.passport = passport;
+    }
+
+    public int getIdNo() {
+        return idNo;
+    }
+
+    public void setIdNo(int idNo) {
+        this.idNo = idNo;
+    }
 }
