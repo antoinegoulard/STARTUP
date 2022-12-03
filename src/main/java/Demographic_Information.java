b2d4a15da6e8b5ba4bf8d28c2aab19d21ca28a39
salahbgm <116580792+salahbgm@users.noreply.github.com>
2022-12-03 18:12:17 +0200

    test

---
+++ src/main/java/Demographic_Information.java
@@ -0,0 +1,25 @@
+public class Demographic_Information {
+    private String race;
+    private String gender;
+
+    public Demographic_Information(String race, String gender) {
+        this.race = race;
+        this.gender = gender;
+    }
+
+    public String getRace() {
+        return race;
+    }
+
+    public void setRace(String race) {
+        this.race = race;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -1,14 +1,17 @@
-public class Employee {
-    int employeeNo;
-    String name;
-    String gende;
-    String race;
+public class Employee extends Demographic_Information {
+    private int employeeNo;
+   private String name;
 
-    public Employee(int employeeNo, String name, String gende, String race) {
+
+    public Employee(int employeeNo, String name, String gender, String race) {
+        super(race, gender);
         this.employeeNo = employeeNo;
         this.name = name;
-        this.gende = gende;
-        this.race = race;
+
+
     }
+
+
+
 
 }
