1807069a00f2c054f5f33cf6f578ddf03de8ec97
Antoine Goulard <antoine.goulard@outlook.com>
2022-12-11 23:01:32 +0200

    class

--- src/main/java/fr/efrei/util/Helper.java
+++ src/main/java/fr/efrei/util/Helper.java
@@ -64,6 +64,53 @@ public class Helper
         Scanner id = new Scanner(System.in);
 
 
+        --------------
+
+                System.out.println("Enter the name of the employee : ");
+        Scanner nom = new Scanner(System.in);
+
+        System.out.println("Enter the Number of the employee : ");
+        Scanner emplNo = new Scanner(System.in);
+
+        System.out.println("Enter the contact of the employee : ");
+        System.out.println("Phone number : ");
+        Scanner contctphone = new Scanner(System.in);
+        System.out.println("Email : ");
+        Scanner contctmail = new Scanner(System.in);
+        System.out.println("Home Number: ");
+        Scanner contcthomeNo = new Scanner(System.in);
+
+        System.out.println("Enter the address of the employee : ");
+        System.out.println("Street Address : ");
+        Scanner address_street = new Scanner(System.in);
+        System.out.println("Postal Address : ");
+        Scanner address_postal = new Scanner(System.in);
+
+
+        System.out.println("Enter the job of the employee : ");
+        System.out.println("Title : ");
+        Scanner post1 = new Scanner(System.in);
+        System.out.println(" Position Code : ");
+        Scanner post2 = new Scanner(System.in);
+        System.out.println("Statue : ");
+        Scanner post3 = new Scanner(System.in);
+
+
+
+        System.out.println("Enter the demographic information of the employee : ");
+        System.out.println("Race : ");
+        Scanner deminfo1 = new Scanner(System.in);
+        System.out.println("Gender : ");
+        Scanner deminfo2 = new Scanner(System.in);
+
+
+        System.out.println("Enter the identity of the employee : ");
+        System.out.println("Passport : ");
+        Scanner id1 = new Scanner(System.in);
+        System.out.println("ID Number : ");
+        Scanner id2 = new Scanner(System.in);
+
+
 
 
 
